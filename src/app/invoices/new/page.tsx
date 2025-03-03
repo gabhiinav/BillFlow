68a8b8c6a2444fa517a0ea4ded5d8506b1c11cfa
gabhiinav <gabhiinav@gmail.com>
2025-03-04 01:19:57 +0530

    database setup

--- src/app/invoices/new/page.tsx
+++ src/app/invoices/new/page.tsx
@@ -3,12 +3,19 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 
-export default function Home() {
+import { sql } from "drizzle-orm";
+import { db } from "@/db";
+
+export default async function Home() {
+  const result = await db.execute(sql`SELECT current_database()`);
+  console.log(result)
   return (
     <main className="flex flex-col justify-center h-full gap-6 max-w-5xl mx-auto my-12">
       <div className="flex justify-between">
         <h1 className="text-3xl font-bold"> Create Invoice</h1>
       </div>
+
+      {JSON.stringify(result)}
 
       <form className="grid gap-4 max-w-xs">
         <div>
