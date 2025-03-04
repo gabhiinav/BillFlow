18f1eaee9591a3e98229367aa00372ca0ce7f3aa
gabhiinav <gabhiinav@gmail.com>
2025-03-04 23:14:32 +0530

    update invoice status

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,10 +1,12 @@
 "use server";
 
 import { db } from "@/db";
-import { Invoices } from "@/db/schema";
+import { Invoices, Status } from "@/db/schema";
 import { redirect } from "next/navigation";
 
 import { auth } from "@clerk/nextjs/server";
+import { and, eq, isNull } from "drizzle-orm";
+import { revalidatePath } from "next/cache";
 
 export async function createAction(formData: FormData) {
   const {userId} = auth();
@@ -28,5 +30,40 @@ export async function createAction(formData: FormData) {
 }
 
 export async function updateStatusAction(formData: FormData) {
-  
+  const { userId, orgId } = auth();
+
+  // Updating disabled for demo
+  if ( userId !== process.env.ME_ID ) return;
+
+  if (!userId) {
+    return;
+  }
+
+  const id = formData.get("id") as string;
+  const status = formData.get("status") as Status;
+
+  if (orgId) {
+    await db
+      .update(Invoices)
+      .set({ status })
+      .where(
+        and(
+          eq(Invoices.id, Number.parseInt(id)),
+          eq(Invoices.organizationId, orgId),
+        ),
+      );
+  } else {
+    await db
+      .update(Invoices)
+      .set({ status })
+      .where(
+        and(
+          eq(Invoices.id, Number.parseInt(id)),
+          eq(Invoices.userId, userId),
+          isNull(Invoices.organizationId),
+        ),
+      );
+  }
+
+  revalidatePath(`/invoices/${id}`, "page");
 }
