77dd8d9b3575b33122072175a048d1a7a256b22d
gabhiinav <gabhiinav@gmail.com>
2025-03-04 18:57:51 +0530

    dropdown status change

--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -1,4 +1,12 @@
+import Container from "@/components/Container";
 import { Badge } from "@/components/ui/badge";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
 import { db } from "@/db";
 import { Invoices } from "@/db/schema";
 import { cn } from "@/lib/utils";
@@ -37,56 +45,59 @@ export default async function InvoicePage({
     notFound();
   }
   return (
-    <main className="h-full max-w-5xl mx-auto my-12">
-      <div className="flex justify-between mb-8">
-        <h1 className="flex items-center gap-4 text-3xl font-bold">
-          Invoice {invoiceId}
-          <Badge
-            className={cn(
-              "rounded-full capitalize",
-              result.status === "open" && "bg-blue-500",
-              result.status === "paid" && "bg-green-600",
-              result.status === "void" && "bg-zinc-700",
-              result.status === "uncollectable" && "bg-red-600"
-            )}
-          >
-            {result.status}
-          </Badge>
-        </h1>
-        <p></p>
-      </div>
+    <main className="w-full h-full">
+      <Container>
+        <div className="flex justify-between mb-8">
+          <h1 className="flex items-center gap-4 text-3xl font-bold">
+            Invoice {invoiceId}
+            <Badge
+              className={cn(
+                "rounded-full capitalize",
+                result.status === "open" && "bg-blue-500",
+                result.status === "paid" && "bg-green-600",
+                result.status === "void" && "bg-zinc-700",
+                result.status === "uncollectable" && "bg-red-600"
+              )}
+            >
+              {result.status}
+            </Badge>
+          </h1>
+          <p></p>
+        </div>
 
-      <p className="text-3xl mb-3">${(result.value / 100).toFixed(2)}</p>
-      <p className="text-lg mb-8">{result.description}</p>
+        <p className="text-3xl mb-3">${(result.value / 100).toFixed(2)}</p>
+        <p className="text-lg mb-8">{result.description}</p>
 
-      <h2 className="font-bold text-lg mb-4">Billing Details</h2>
+        <h2 className="font-bold text-lg mb-4">Billing Details</h2>
 
-      <ul className="grid gap-2">
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Invoice ID {result.id}
-          </strong>
-          <span></span>
-        </li>
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Invoice Date {new Date(result.createTs).toLocaleDateString("en-GB")}
-          </strong>
-          <span></span>
-        </li>
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Billing Name
-          </strong>
-          <span></span>
-        </li>
-        <li className="flex gap-4">
-          <strong className="block w-28 flex-shrink-0 font-medium text-sm">
-            Billing Email
-          </strong>
-          <span></span>
-        </li>
-      </ul>
+        <ul className="grid gap-2">
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice ID {result.id}
+            </strong>
+            <span></span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Invoice Date{" "}
+              {new Date(result.createTs).toLocaleDateString("en-GB")}
+            </strong>
+            <span></span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Name
+            </strong>
+            <span></span>
+          </li>
+          <li className="flex gap-4">
+            <strong className="block w-28 flex-shrink-0 font-medium text-sm">
+              Billing Email
+            </strong>
+            <span></span>
+          </li>
+        </ul>
+      </Container>
     </main>
   );
 }
